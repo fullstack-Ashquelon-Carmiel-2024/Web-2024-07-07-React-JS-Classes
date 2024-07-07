69b2860911c5b839db57d19529427b9eb2de89e7
NatalieShif <natalia.shif@gmail.com>
2024-07-07 09:06:49 +0300

    first commit

---
+++ projects/10-hanoi-towers/script.js
@@ -0,0 +1,107 @@
+const towerList = document.querySelectorAll('.tower');
+const main = document.querySelector('main');
+
+// TBD - create listener and get the number of disks, 
+//       limit the number of disks
+let numOfDisks = 4, maxWidth = 180, minus = 20, diskHeight = 20, towerBottomMargin = 10; 
+// TBD - above some things that should not be hardcoded
+// diskHeight is also a tower base height
+const disks = [];
+/* JS objects are built by Prototypes - they're actually
+enveloped now by the syntax of classes */
+
+/********* Get Random Color ***********/
+const getRand0To255 = () => Math.floor(Math.random() * 256);
+
+const getColor = () => `rgb(${getRand0To255()},${getRand0To255()},${getRand0To255()})`;
+
+/********* Classes ***********/
+class Tower {
+
+    constructor(elTower) {
+
+        this.disks = []
+        this.elTower = elTower;
+
+    }
+
+    findMiddle() {
+        // The leftmost point + half of the width
+        this.middle = this.elTower.offsetLeft + this.elTower.offsetWidth / 2;
+        console.log(`This.middle = ${this.middle}`);
+
+    }
+
+    removeDisk() {
+        return this.disks.pop();
+    }
+
+    addDisk(disk) {
+        // tbd: if we could add this disk here
+        // tbd: set x and y of the disk - get for this "this.middle"
+        disk.setX(this.middle);
+        disk.setY((this.disks.length + 1) * diskHeight + towerBottomMargin);
+        this.disks.push(disk);
+    }
+
+}
+
+class Disk {
+
+    constructor(elDisk,width) {
+        this.elDisk = elDisk;
+        this.elDisk.style.backgroundColor = getColor();
+        this.width = width;
+        this.elDisk.style.width = this.width + 'px';
+    }
+
+    setX(towerMiddle) {
+        console.log(`towerMiddle=${towerMiddle}`)
+        console.log(`this.width=${this.width}`)
+        this.x = towerMiddle - this.width / 2; 
+        this.elDisk.style.left = this.x + 'px';
+    }
+    
+    setY(currentHeight) {
+        // tbd
+        this.y = currentHeight;
+        this.elDisk.style.bottom = this.y + 'px';
+    }
+
+}
+
+/*********************** Create Tower Objects ***************************/
+
+const towers = Array.from(towerList).map(el => {
+    
+    // const currDate = new Date();
+    // const endOfMay = new Date('2024-05-31');
+    // Operator "new": 1. Gets location in the memory and now "newTower"
+    //                     points to this location in the memory
+    //                 2. Calls class Tower constructor function 
+    //                 3. Points "this" to the current new location in the memory
+    const newTower = new Tower(el);
+    newTower.findMiddle();
+    return newTower;
+    
+}) 
+
+/*********************** Create Disk Objects ***************************/
+for (let i = 0; i < numOfDisks; i++) {
+
+    const newDiskEl = document.createElement('div');
+    newDiskEl.classList.add('disk');
+    main.append(newDiskEl);
+
+    // Calculate width:
+    // i = 0 width = 180 = maxWidth - minus * i
+    // i = 1 width = 160 = maxWidth - minus * i
+    // i = 2 width = 140 = maxWidth - minus * i
+    // i = 3 width = 120 = maxWidth - minus * i 
+
+    const newDisk = new Disk(newDiskEl, maxWidth - minus * i);
+
+    disks.push(newDisk);
+    towers[0].addDisk(newDisk);
+
+}
---
+++ projects/11-shopping-carts/script.js
@@ -0,0 +1,50 @@
+const avatarStockEl = document.querySelector(`.customer-avatars .stock`);
+const cartStockEl = document.querySelector(`.carts .stock`);
+
+const getRandColor = () => {
+    const getRand0To255 = () => Math.floor(Math.random()*256);
+    return `rgb(${getRand0To255()},${getRand0To255()},${getRand0To255()})`;
+}
+
+class Stock {
+    constructor(iconList,el) {
+        this.el = el;
+        this.creStock(iconList);
+    }
+
+    creStock(iconList) {
+        const toShuffle = [];
+        for (let i = 0; i < iconList.length; i++) {
+                     toShuffle.push(i)} 
+        
+        this.stock = iconList.map(i => {
+            
+            const newIcon = document.createElement('i');
+            this.el.append(newIcon);
+            let ix = Math.floor(Math.random() * toShuffle.length);
+            newIcon.style.order = toShuffle[ix];
+            toShuffle.splice(ix,1);
+            return new Avatar(i,newIcon);
+        })
+    }
+}
+
+class Avatar {
+    constructor(iconName,el) {
+        this.iconName = iconName;
+        this.el = el;
+        this.el.style.color = getRandColor();
+        
+        this.el.className = iconName;
+        this.el.classList.add('p-2');
+        
+    }
+
+    changeChosen() {
+
+    }
+}
+
+const avatarStock = new Stock(avatarIconNames,avatarStockEl);
+const cartStock = new Stock(cartIconNames,cartStockEl);
+
